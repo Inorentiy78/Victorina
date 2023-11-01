9e7d22bfd905ca8aacfd2182b41af050f83573fd
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2023-11-01 14:56:12 +0500

    Create codirok.py

---
+++ codirok.py
@@ -0,0 +1,28 @@
+alfavit = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.+-/+_()&^%$#@!?><\""
+
+def CodeEncode(text: str, k: int):
+    # добавляем в алфавит маленькие буквы
+    fullAlfavit = alfavit + alfavit.lower()
+    letterQty = len(fullAlfavit)
+    retVal = ""
+    for item in text:
+        index = fullAlfavit.find(item)
+        if item not in fullAlfavit:
+            retVal += item
+        else:
+            codeIndex = (letterQty + index + k) % letterQty
+            retVal += fullAlfavit[codeIndex]
+
+    return retVal
+
+# Чтение вопросов из файла
+with open("vopros.txt", "r", encoding="utf-8") as file:
+    voprosy = file.read()
+
+key = 2
+encryptedQuestions = CodeEncode(voprosy, key)
+
+# Запись зашифрованных вопросов в файл "newshifr.txt"
+with open("newshifr.txt", "w", encoding="utf-8") as file:
+    file.write(encryptedQuestions)
+print(voprosy)
