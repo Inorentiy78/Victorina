90fa9856ec63d45e0ce4414fbe23fb7b406b079d
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2023-11-02 01:26:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,29 @@
+код с ошибкой
 import random
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QRadioButton, QMessageBox
-
-class QuestionParser:
-    def __init__(self, questions_file, answers_file):
-        self.questions_file = questions_file
-        self.answers_file = answers_file
-
-    def read_questions_and_answers(self):
-        questions = []
-        with open(self.questions_file, "r", encoding="utf-8") as questions_file:
-            question_data = questions_file.read().split("---\n")
-
-        with open(self.answers_file, "r", encoding="utf-8") as answers_file:
-            answer_data = answers_file.read().split("\n")
-
-        for question_block, answer_line in zip(question_data, answer_data):
-            question_lines = question_block.split("\n")
-            question = {
-                "question": question_lines[0],
-                "options": question_lines[1:5],
-                "correct_answer": int(answer_line.split(": ")[1])
-            }
-            questions.append(question)
-
-        # Shuffle the questions and select the first 25
-        random.shuffle(questions)
-        selected_questions = questions[:25]
-
-        return selected_questions
-
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QRadioButton, QMessageBox, QTextBrowser
 
 class QuizApp(QWidget):
     def __init__(self, questions):
         super().__init__()
         self.setWindowTitle('Викторина')
         self.setGeometry(100, 100, 400, 300)
-        self.current_question = 0
-        self.score = 0
         self.questions = questions
         self.init_ui()
 
+        self.correct_answers = []  # Для отслеживания правильных ответов
+        self.results_window = None
+
     def init_ui(self):
+        self.current_question = 0
+        self.score = 0
 
         self.question_label = QLabel()
         self.radio_buttons = []
         self.submit_button = QPushButton('Ответить')
-        self.submit_button.setEnabled(False)  # Начально 
-        
+        self.submit_button.setEnabled(False)  # Начально
+
         self.question_indices = list(range(len(self.questions)))
-        random.shuffle(self.question_indices)
-        self.current_question_number = 0
 
         layout = QVBoxLayout()
         layout.addWidget(self.question_label)
@@ -62,25 +35,19 @@ class QuizApp(QWidget):
         layout.addWidget(self.submit_button)
         self.setLayout(layout)
 
-        self.submit_button.clicked.connect(self.check_answer)  # Подключаем обработчик к кнопке "Ответить"
-
+        self.submit_button.clicked.connect(self.check_answer)
         self.show_question()
 
-    
-
     def show_question(self):
-        if self.current_question_number < 25:  # Display and score only 25 questions
-            if not self.question_indices:
-                self.show_result()
-                return
-
-            # Get the next random question index
-            random_question_index = self.question_indices.pop()
+        if self.current_question < len(self.questions):
+            random_question_index = self.question_indices.pop(0)  # Извлекаем первый вопрос
             question = self.questions[random_question_index]
 
-            # Display the question number and text
-            self.question_label.setText(f' {question["question"]}')
+            # Оставляем порядок вариантов ответов неизменным
             options = question["options"]
+
+            self.question_label.setText(f' {question["question"]}')
+
             for i, radio_button in enumerate(self.radio_buttons):
                 if i < len(options):
                     radio_button.setText(options[i])
@@ -88,6 +55,7 @@ class QuizApp(QWidget):
                     radio_button.setVisible(True)
                 else:
                     radio_button.setVisible(False)
+
             self.submit_button.setEnabled(False)
         else:
             self.show_result()
@@ -102,15 +70,61 @@ class QuizApp(QWidget):
                 selected_option = i
 
         correct_answer = self.questions[self.current_question]["correct_answer"]
-        if selected_option == correct_answer:
-            self.score += 1
+        is_correct = (selected_option == correct_answer)
+        self.correct_answers.append(is_correct)  # Записываем результат
 
         self.current_question += 1
-        self.show_question()
+
+        if self.current_question == len(self.questions):
+            self.show_result()
+        else:
+            self.show_question()
+
+        self.update_score()  # Обновление отображаемого счета
+
+    def update_score(self):
+        correct_count = sum(self.correct_answers)  # Считаем количество правильных ответов
+        self.score = correct_count  # Обновляем значение self.score
 
     def show_result(self):
-        QMessageBox.information(self, 'Результаты', f'Вы набрали {self.score} из {len(self.questions)} баллов.')
-        sys.exit()
+        result_message = 'Результаты:\n\n'
+
+        for i, (question, is_correct) in enumerate(zip(self.questions, self.correct_answers)):
+            result_message += f'Вопрос {i + 1}: {question["question"]}\n'
+            if is_correct:
+                result_message += 'Ответ: Правильно\n\n'
+            else:
+                correct_answer_index = question["correct_answer"]
+                if 0 <= correct_answer_index < len(question["options"]):
+                    result_message += f'Ответ: Неправильно\n'
+                    result_message += f'Правильный ответ: {question["options"][correct_answer_index]}\n\n'
+                else:
+                    result_message += 'Ответ: Неправильно\n'
+                    result_message += 'Правильный ответ: Отсутствует в вариантах ответов\n\n'
+
+        if self.results_window is None:
+            self.results_window = ResultsWindow(result_message)
+            self.results_window.show()
+        else:
+            self.results_window.append_results(result_message)
+
+
+class ResultsWindow(QWidget):
+    def __init__(self, initial_results):
+        super().__init__()
+        self.setWindowTitle('Результаты')
+        self.setGeometry(200, 200, 400, 600)
+        self.results_text = QTextBrowser()
+        self.results_text.setPlainText(initial_results)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.results_text)
+        self.setLayout(layout)
+
+    def append_results(self, result_message):
+        current_text = self.results_text.toPlainText()
+        self.results_text.setPlainText(current_text + result_message)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
